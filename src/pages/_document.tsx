fc2b43a39aabba9ae1c1d104d6439fcaaf9f070a
notRyuk <amadeusrobot.notryuk@gmail.com>
2024-01-05 16:08:54 +0530

    [client] initial setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import Footer from '@components/footer'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -5,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <Footer />
         <Main />
         <NextScript />
       </body>
---
+++ src/types_/response.ts
@@ -0,0 +1,7 @@
+export type ResponseType = string|object|string[]|object[]
+
+export enum ResponseStatus {
+    success = "success",
+    pending = "pending",
+    error = "error"
+}

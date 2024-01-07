eaeee277af7efbbd20cae424bd2d12bd666691a0
notRyuk <amadeusrobot.notryuk@gmail.com>
2024-01-07 07:58:52 +0530

    [server] add user roles

--- src/types_/user/index.ts
+++ src/types_/user/index.ts
@@ -3,10 +3,20 @@ import { prop } from "@typegoose/typegoose";
 import { IMongoDocument } from "@types_/mongo";
 import Models from "@utils/models";
 import { IsPhone } from "@utils/validator";
-import { IsEmail } from "class-validator";
-import { Field, ObjectType } from "type-graphql";
+import { IsEmail, IsEnum } from "class-validator";
+import { Field, ObjectType, registerEnumType } from "type-graphql";
 
 const handler = new ErrorHandler(Models.user)
+
+export enum UserTypes {
+    participant = "participant",
+    admin = "admin",
+    clubLead = "club-lead",
+}
+
+registerEnumType(UserTypes, {
+    name: "UserType"
+})
 
 @ObjectType()
 export default class IUser extends IMongoDocument {
@@ -40,6 +50,14 @@ export default class IUser extends IMongoDocument {
     @Field(() => String)
     @prop({ required: handler.fieldRequired("college") })
     college!: string
+
+    @IsEnum(UserTypes)
+    @Field(() => UserTypes)
+    @prop({
+        default: UserTypes.participant,
+        enum: Object.values(UserTypes)
+    })
+    type!: UserTypes
 }
 
 export type UserType = typeof IUser

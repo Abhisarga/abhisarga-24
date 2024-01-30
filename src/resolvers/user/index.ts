20b4448f0fb5753927ac804f67abbe4a2071c559
aahnik <daw@aahnik.dev>
2024-01-30 12:48:46 +0530

    add CreatePerson resolver

--- src/resolvers/user/index.ts
+++ src/resolvers/user/index.ts
@@ -2,10 +2,11 @@ import ErrorHandler from "@handlers/error";
 import Person from "@models/user/person";
 import { IResponse } from "@types_/response";
 import IUser from "@types_/user";
+import IPerson from "@types_/user/person";
 import User from "@models/user"
 import Models from "@utils/models";
 import { Types } from "mongoose";
-import { Arg, Query, Resolver } from "type-graphql";
+import { Arg, Query, Resolver , Mutation} from "type-graphql";
 
 @Resolver(() => IUser)
 export default class UserResolver {
@@ -36,4 +37,17 @@ export default class UserResolver {
         }
         return this.handler.success(person)
     }
+
+    @Mutation(() => IPerson)
+    async CreatePerson(
+        @Arg("person", () => IPerson) input: IPerson) {
+        delete input._id
+        delete input.__v
+
+        const person = await Person.create(input)
+        if (!person) {
+            return this.handler.error(null)
+        }
+        return this.handler.success(person)
+  }
 }
